21a1dc0f2b6db6d1ae5184fa22da82425d3177c7
MinhTri <vominhtri270496@gmail.com>
2022-05-31 22:51:11 +0700

    'commit_2'

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import DatePickerDetail from '../Detail/DatePickerDetail'
+import DemoDateRangePickerMUI from '../Detail/DemoDateRangePickerMUI'
 
 
 export default function Home() {
   return (
     <div>
-      <DatePickerDetail></DatePickerDetail>
+      <DemoDateRangePickerMUI></DemoDateRangePickerMUI>
     </div>
   )
 }
